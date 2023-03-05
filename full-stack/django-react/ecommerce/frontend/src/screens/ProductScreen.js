53339c049d6a143dd71271212f7f838322dd9df6
991GT3RS <119116065+growth-delta@users.noreply.github.com>
2023-03-05 21:15:20 +0000

    backend
    backend build: REST API

--- full-stack/django-react/ecommerce/frontend/src/screens/ProductScreen.js
+++ full-stack/django-react/ecommerce/frontend/src/screens/ProductScreen.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+// import products from '../products'
+import axios from 'axios'
 
 function ProductScreen() {
+    const[product, setProduct]  = useState([])
     const { id } = useParams();
-    const product = products.find((p) => p._id === id);
+  
+    useEffect(() => {
+      async function fetchProduct(){
+        const { data } = await axios.get(`/api/products/${id}`)
+        setProduct(data)
+      }
+      fetchProduct()
+    }, [])
+    // const { id } = useParams();
+    // const product = products.find((p) => p._id === id);
     return (
         <div>
             <h1 className='text-center'>{product.name}</h1>
