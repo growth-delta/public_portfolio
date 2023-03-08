e721e428a7b183d63d2b863a112eae9863eb0817
991GT3RS <119116065+growth-delta@users.noreply.github.com>
2023-03-08 15:48:22 +0000

    v1

--- full-stack/django-react/ecommerce/frontend/src/screens/ProductScreen.js
+++ full-stack/django-react/ecommerce/frontend/src/screens/ProductScreen.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-// import products from '../products'
 import axios from 'axios'
 
 function ProductScreen() {
---
+++ full-stack/django-react/ecommerce/frontend/src/store.js
@@ -0,0 +1,37 @@
+// import { createStore, combineReducers, applyMiddleware } from 'redux'
+// import { configureStore } from '@reduxjs/toolkit';
+// import thunk from 'redux-thunk'
+// import { composeWithDevTools } from 'redux-devtools-extension'
+
+// const reducer = combineReducers({})
+
+// const initialState = {}
+
+// const middleware = [thunk]
+
+// const store = configureStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+// export default store
+import { combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducer } from './reducers/productReducers';
+
+const reducers = combineReducers({
+  productList: productListReducer,
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = configureStore({
+  reducer: reducers, // Add your reducers here
+  preloadedState: initialState,
+  middleware: middleware,
+  devTools: true, // Enable Redux DevTools
+});
+
+export default store;
+
