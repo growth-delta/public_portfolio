bbcc5ed6047b99218679cf77a10146e40a9d1d30
991GT3RS <119116065+growth-delta@users.noreply.github.com>
2023-03-08 17:09:16 +0000

    SECTION 4 COMPLETE

--- full-stack/django-react/ecommerce/frontend/src/screens/ProductScreen.js
+++ full-stack/django-react/ecommerce/frontend/src/screens/ProductScreen.js
@@ -1,70 +1,89 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProductDetails } from '../actions/productActions'
+// import axios from 'axios'
 
 function ProductScreen() {
-    const[product, setProduct]  = useState([])
+    // const[product, setProduct]  = useState([])
+    // const { id } = useParams();
+  
+    // useEffect(() => {
+    //   async function fetchProduct(){
+    //     const { data } = await axios.get(`/api/products/${id}`)
+    //     setProduct(data)
+    //   }
+    //   fetchProduct()
+    // }, [])
+    // // const { id } = useParams();
+    // // const product = products.find((p) => p._id === id);
+
     const { id } = useParams();
+    const dispatch = useDispatch();
+    const productDetails = useSelector(state => state.productDetails);
+    const { loading, error, product } = productDetails;
   
     useEffect(() => {
-      async function fetchProduct(){
-        const { data } = await axios.get(`/api/products/${id}`)
-        setProduct(data)
-      }
-      fetchProduct()
-    }, [])
-    // const { id } = useParams();
-    // const product = products.find((p) => p._id === id);
+        dispatch(listProductDetails(id))
+    }, [dispatch, id]);
+
     return (
         <div>
             <h1 className='text-center'>{product.name}</h1>
             <Link to='/'><i className='fa-solid fa-circle-left text-secondary fa-3x'></i></Link>
             <br></br><br></br>
-            <Row>
-                <Col md={6}>
-                    <Image src={product.image} alt={product.name} fluid />
-                </Col>
-                <Col md={3}>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <b>{product.name}</b>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Rating value={product.rating} text={`${product.numReviews} Reviews`} color={'#f8e825'} />
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Price: <i className="fa-solid fa-dollar-sign"></i> {product.price}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            {product.description}
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-                    <Card>
+            {loading ? <Loader />: 
+            error ? <Message variant='danger'>{error}</Message>:
+            (
+                <Row>
+                    <Col md={6}>
+                        <Image src={product.image} alt={product.name} fluid />
+                    </Col>
+                    <Col md={3}>
                         <ListGroup variant='flush'>
                             <ListGroup.Item>
-                                <Row>
-                                    <Col>Price: </Col>
-                                    <Col><strong>${product.price}</strong></Col>
-                                </Row>
+                                <b>{product.name}</b>
                             </ListGroup.Item>
                             <ListGroup.Item>
-                                <Row>
-                                    <Col>Status: </Col>
-                                    <Col><strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong></Col>
-                                </Row>
-                                <br></br>
-                                <Row>
-                                    <Button className='btn btn-success btn-block' disabled={product.countInStock === 0} type='button'>Add to Cart</Button>
-                                </Row>
-                            </ListGroup.Item> 
+                                <Rating value={product.rating} text={`${product.numReviews} Reviews`} color={'#f8e825'} />
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Price: <i className="fa-solid fa-dollar-sign"></i> {product.price}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                {product.description}
+                            </ListGroup.Item>
                         </ListGroup>
-                    </Card>  
-                </Col>
-            </Row>
+                    </Col>
+                    <Col md={3}>
+                        <Card>
+                            <ListGroup variant='flush'>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Price: </Col>
+                                        <Col><strong>${product.price}</strong></Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Status: </Col>
+                                        <Col><strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong></Col>
+                                    </Row>
+                                    <br></br>
+                                    <Row>
+                                        <Button className='btn btn-success btn-block' disabled={product.countInStock === 0} type='button'>Add to Cart</Button>
+                                    </Row>
+                                </ListGroup.Item> 
+                            </ListGroup>
+                        </Card>  
+                    </Col>
+                </Row>
+            )
+            }
         </div>
     )
 };
--- full-stack/django-react/ecommerce/frontend/src/store.js
+++ full-stack/django-react/ecommerce/frontend/src/store.js
@@ -16,10 +16,11 @@ import { combineReducers } from 'redux'
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 // import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './reducers/productReducers';
+import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 
 const reducers = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const initialState = {};
